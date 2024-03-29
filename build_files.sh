9a59bb4bbb5a694f38d5606c7046c1c80fb147c5
noahwekesa <noahwekesa0@gmail.com>
2024-03-30 00:34:32 +0300

    noma

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# install requirements
-python3 -m pip install -r requirements.txt
-
-# migrations and migrate
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
-
-# collectstatic
-python3 manage.py collectstatic --noinput
+pip install -r requirements.txt
+python manage.py collectstatic
