a09ac2c3b2246f6234452e1c174f873e5071ed00
noahwekesa <noahwekesa0@gmail.com>
2024-03-30 00:53:31 +0300

    i

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # install requirements
-python3.9 -m pip install -r requirements.txt
-
-python3.9 manage.py collectstatic --noinput
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
