ea7749292d742e2d5d73ecc72cb8e9371eac583a
noahwekesa <noahwekesa0@gmail.com>
2024-03-30 00:36:41 +0300

    ok

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,24 +3,31 @@ from django.contrib import admin
 from django.urls import path, include
 from projects import views
 
-urlpatterns = [
-    path(
-        "activate/project/<slug:handle>",
-        views.activate_project_view,
-    ),
-    path(
-        "deactivate/project/<slug:handle>",
-        views.deactivate_project_view,
-    ),
-    path("admin/", admin.site.urls),
-    # Packages
-    path("accounts/", include("allauth.urls")),
-    path("__reload__/", include("django_browser_reload.urls")),
-    # apps
-    path("", include("pages.urls")),
-    path("projects/", include("projects.urls")),
-    path("items/", include("items.urls")),
-]
+from django.conf.urls.static import static
+
+urlpatterns = (
+    [
+        path(
+            "activate/project/<slug:handle>",
+            views.activate_project_view,
+        ),
+        path(
+            "deactivate/project/<slug:handle>",
+            views.deactivate_project_view,
+        ),
+        path("admin/", admin.site.urls),
+        # Packages
+        path("accounts/", include("allauth.urls")),
+        path("__reload__/", include("django_browser_reload.urls")),
+        # apps
+        path("", include("pages.urls")),
+        path("projects/", include("projects.urls")),
+        path("items/", include("items.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
+
 
 if settings.DEBUG:
     import debug_toolbar
