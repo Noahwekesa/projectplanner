a42f76330aa1a3c17ec073f21b0e5b3ad3d5861c
noahwekesa <noahwekesa0@gmail.com>
2024-03-30 00:46:07 +0300

    hc

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-python3 pip install -r requirements.txt
+# install requirements
+python3 -m pip install -r requirements.txt
 
+# migrations and migrate
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+# collectstatic
 python3 manage.py collectstatic --noinput
