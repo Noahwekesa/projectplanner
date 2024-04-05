cbca9e2c27be5e7a4796ce1e1d5058746f374049
noahwekesa <noahwekesa0@gmail.com>
2024-04-06 00:46:15 +0300

    chnages

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-# Upgrade pip
-python3 -m pip install --upgrade pip
-
-# Install requirements
+# install requirements
 python3 -m pip install -r requirements.txt
 
-# Install tailwind
-python3 manage.py tailwind install
+# migrations and migrate
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
-# Collect static files
+# collectstatic
 python3 manage.py collectstatic --noinput
