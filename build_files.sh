d7d003e907476b203a91af2a78f436686edf47f0
noahwekesa <noahwekesa0@gmail.com>
2024-04-06 00:33:43 +0300

    fixe

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-python3 pip install --upgrade pip
-# install requirements
-python3 -m pip install -r requirements.txt
-pyython3 -m python manage.py tailwind install
+# Upgrade pip
+python3 -m pip install --upgrade pip
 
-# collectstatic
+# Install requirements
+python3 -m pip install -r requirements.txt
+
+# Install tailwind
+python3 manage.py tailwind install
+
+# Collect static files
 python3 manage.py collectstatic --noinput
