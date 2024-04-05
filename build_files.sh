11a8b58755c3e484fb2fa9c16818cef42f1b88c7
noahwekesa <noahwekesa0@gmail.com>
2024-04-06 00:52:23 +0300

    up

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# install requirements
+# Install requirements
+python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
-# migrations and migrate
+# Migrations and migrate
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
-# collectstatic
+# Collectstatic
 python3 manage.py collectstatic --noinput
